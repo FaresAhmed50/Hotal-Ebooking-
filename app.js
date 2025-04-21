9c8538bdd178e1b5e453300d29e41b04ca2d5c88
fares <fares.ahmed15967@aiet.edu.eg>
2025-04-21 23:47:42 +0200

    Edit the app.js

--- app.js
+++ app.js
@@ -16,14 +16,19 @@ app.use('/api/auth', authRoutes);
 app.use('/api/user', userRoutes);
 app.use('/api/admin', adminRoutes);
 
-// Error handling
+
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).json({ message: 'Something went wrong!' });
 });
 
-// Start server
+
 const PORT = process.env.PORT || 5000;
+
+
+console.log(`PORT from env: ${process.env.PORT}`);
+
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
