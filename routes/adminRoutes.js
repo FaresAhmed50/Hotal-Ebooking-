1b0d585cfa1e2aabd13032af0ae946b586a1955d
Mohamed Hisham <mohamed.hishamk90@gmail.com>
2025-04-27 10:43:19 +0300

    Delete routes/adminRoutes.js

--- routes/adminRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const adminController = require('../controllers/adminController');
-const authMiddleware = require('../middlewares/authMiddleware');
-const isAdmin = require('../middlewares/isAdmin');
-
-router.post('/hotels', authMiddleware, isAdmin, adminController.createHotel);
-
-module.exports = router;
