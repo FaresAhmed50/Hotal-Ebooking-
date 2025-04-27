c684623e4097cd426d69b399d1cb01750a4c3fde
Mohamed Hisham <mohamed.hishamk90@gmail.com>
2025-04-27 10:05:50 +0300

    final

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -7,5 +7,8 @@ const isAdmin = require('../middlewares/isAdmin');
 router.post('/register', authController.register);
 router.post('/login', authController.login);
 router.post('/set-admin', authMiddleware, isAdmin, authController.setAdmin);
+router.put('/users/:id', authMiddleware, isAdmin, authController.updateUser);
+router.delete('/users/:id', authMiddleware, isAdmin, authController.deleteUser);
+router.get('/users/by-email', authMiddleware, isAdmin, authController.getUserByEmail);
 
 module.exports = router;
